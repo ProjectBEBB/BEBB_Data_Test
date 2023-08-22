a15bd9878401b86c4d2bca753bcade6dc81d66fe
Sulamith Gehr <sulamith.gehr@unibas.ch>
2023-08-22 15:38:50 +0200

    Revert "Data has been updated"
    This reverts commit 9f656367e67e7ccab847459ae727516aa16c4971.

--- index/places/includeFiles.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-a = open("output.txt", "a")
-for path, subdirs, files in os.walk(r'/Users/l-gehsul00/Documents/BEBB-Github/Ueberarbeitung_Editionsrichtlinien/teiCorpus/index/places'):
-   for filename in files:
-     f = os.path.join(path, filename)
-     a.write(str(f) + os.linesep) 
